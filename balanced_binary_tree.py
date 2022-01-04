de113ed0b56cf68ea0f139003ddca80b4f1d8844
shivanshu shukla <69464466+myhackera@users.noreply.github.com>
2022-01-05 00:34:31 +0530

    Create balanced_binary_tree.py

---
+++ balanced_binary_tree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def height(self, root):
+        if root is None:
+            return 0
+        return max(self.height(root.left), self.height(root.right))+1
+    
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if root is None:
+            return True
+        lh = self.height(root.left)
+        rh = self.height(root.right)
+        if (abs(lh-rh)<=1) and self.isBalanced(root.left) is True and self.isBalanced(root.right) is True:
+            return True
+        return False
+            
